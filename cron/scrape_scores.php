1898931a5b8bcd200f15ba9f170da7609814042b
Chris Ward <wardy277@gmail.com>
2016-01-05 19:59:41 +0000

    adding scraping of reviewer score via web (not in api unfortunately)

--- cron/scrape_scores.php
+++ cron/scrape_scores.php
@@ -1,8 +1,11 @@
 <?php
 include(dirname(__FILE__)."/../settings.php");
 
+//setup comic api
+$comicvine = new ComicVine();
+
 //get not scored where there are issues, ordered by newest issue
-$sql = "SELECT v.name, i.id, i.issue_number, i.site_detail_url, score
+$sql = "SELECT v.name, i.id, i.issue_number, i.site_detail_url
 			FROM volumes v
 			JOIN issues i ON i.volume_id = v.comicvine_id
 			WHERE score IS NULL
@@ -16,7 +19,15 @@ foreach($db->getArray($sql) as $row){
 	//$issue = $comicvine->getIssue($issue_id);
 	//$review = $issue['has_staff_review']['site_detail_url'];
 
-	//$review_score = $comicvine->getIssueReview($review);
+	//get score of issue
+	$review_score = $comicvine->getIssueReview($review);
+	if(empty($review_score)){
+		//not yet reviewed flag for later on
+		$review_score = -1;
+	}
+
+	$db->update('issues', array('score'=>$review_score),  array('id'=>$row['id']));
+	//exit;
 
 	//dangerous as can get blacked. try and pretend to be a human
 	echo " ".(rand(50, 300)/100);
